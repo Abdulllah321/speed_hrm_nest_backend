106034440ce444ac1a893d8145a5234ddf469625
Abdulllah321 <abdullahsufyan2007@gmail.com>
2026-01-30 20:23:34 +0500

    .

--- backup/roles/company_roles.sql
+++ backup/roles/company_roles.sql
@@ -2,7 +2,6 @@
 -- PostgreSQL database cluster dump
 --
 
-\restrict ojlYljZFIg1dEjcI4fxeWcwAFkccbYsqfgEhkQQyUFf3ScGFKFFl1LnjKcdOrvU
 
 SET default_transaction_read_only = off;
 
@@ -13,9 +12,9 @@ SET standard_conforming_strings = on;
 -- Roles
 --
 
-CREATE ROLE speedlimit;
+CREATE ROLE IF NOT EXISTS speedlimit;
 ALTER ROLE speedlimit WITH SUPERUSER INHERIT CREATEROLE CREATEDB LOGIN REPLICATION BYPASSRLS PASSWORD 'SCRAM-SHA-256$4096:0OrXNQGVLfOcRl6Y74Vylw==$zhE0bScjUDyPrvlFalpz6apVTaPKzM6nmkNxjamlJe0=:AeDj7du1O3JNGDNFhdA5en9l2/4DyBDwfNl66hfvfXY=';
-CREATE ROLE user_speed_sport_mkzblxzh;
+CREATE ROLE IF NOT EXISTS user_speed_sport_mkzblxzh;
 ALTER ROLE user_speed_sport_mkzblxzh WITH NOSUPERUSER INHERIT NOCREATEROLE NOCREATEDB LOGIN NOREPLICATION NOBYPASSRLS PASSWORD 'SCRAM-SHA-256$4096:kd/STQT50ZAsFwQqHnmJ0A==$cOrBuB6J1XDabnk0l1N/cbCREmlZydFcx7FhB5vCOrI=:XOI2/0wYANkvoQWvua4Cx857Bj8TW/mD2fXDZvMMufA=';
 
 --
