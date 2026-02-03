33e5118fb4682a150ac844799da70dc77d9f652a
muhammad-huzaifa-anwar <muhammadhuzaifa2544@gmail.com>
2026-02-03 10:32:50 +0500

    testingafterMULTIdbs

---
+++ src/finance/payment-voucher/payment-voucher.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { PaymentVoucherService } from './payment-voucher.service';
+import { PaymentVoucherController } from './payment-voucher.controller';
+import { PrismaModule } from '../../prisma/prisma.module';
+
+@Module({
+    imports: [PrismaModule],
+    controllers: [PaymentVoucherController],
+    providers: [PaymentVoucherService],
+})
+export class PaymentVoucherModule { }
