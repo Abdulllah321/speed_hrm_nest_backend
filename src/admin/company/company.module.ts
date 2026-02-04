de88aef4e78afa7d9636a9350b67919843591893
MuhammadUmair1152 <muhammadumair1152@gmail.com>
2026-02-04 20:37:31 +0500

    HRM integration

--- src/admin/company/company.module.ts
+++ src/admin/company/company.module.ts
@@ -2,7 +2,11 @@ import { Module } from '@nestjs/common';
 import { CompanyService } from './company.service';
 import { CompanyController } from './company.controller';
 
+import { forwardRef } from '@nestjs/common';
+import { AuthModule } from '../../auth/auth.module';
+
 @Module({
+    imports: [forwardRef(() => AuthModule)],
   controllers: [CompanyController],
   providers: [CompanyService],
   exports: [CompanyService],
