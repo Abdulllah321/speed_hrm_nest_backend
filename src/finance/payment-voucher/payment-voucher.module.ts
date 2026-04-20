6a2a4598f1c9d0afc6e2a84a3423c6be8acdf457
Abdulllah321 <abdullahsufyan2007@gmail.com>
2026-04-20 08:57:08 -0700

    .

--- src/finance/payment-voucher/payment-voucher.module.ts
+++ src/finance/payment-voucher/payment-voucher.module.ts
@@ -3,9 +3,10 @@ import { PaymentVoucherService } from './payment-voucher.service';
 import { PaymentVoucherController } from './payment-voucher.controller';
 import { PrismaModule } from '../../prisma/prisma.module';
 import { AccountingModule } from '../accounting/accounting.module';
+import { FinanceAccountConfigModule } from '../finance-account-config/finance-account-config.module';
 
 @Module({
-  imports: [PrismaModule, AccountingModule],
+  imports: [PrismaModule, AccountingModule, FinanceAccountConfigModule],
   controllers: [PaymentVoucherController],
   providers: [PaymentVoucherService],
 })
--- src/pos-sales/pos-sales.module.ts
+++ src/pos-sales/pos-sales.module.ts
@@ -3,11 +3,12 @@ import { PosSalesController } from './pos-sales.controller';
 import { PosSalesService } from './pos-sales.service';
 import { DatabaseModule } from '../database/database.module';
 import { StockLedgerModule } from '../warehouse/stock-ledger/stock-ledger.module';
+import { FbrService } from './fbr.service';
 
 @Module({
     imports: [DatabaseModule, StockLedgerModule],
     controllers: [PosSalesController],
-    providers: [PosSalesService],
+    providers: [PosSalesService, FbrService],
     exports: [PosSalesService],
 })
 export class PosSalesModule { }
