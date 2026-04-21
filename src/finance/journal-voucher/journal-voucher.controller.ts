86ad5433fcea046f6d1d2492165a9bc40743aba6
Abdulllah321 <abdullahsufyan2007@gmail.com>
2026-04-21 21:17:15 +0500

    ERP Permissions done

--- src/finance/journal-voucher/journal-voucher.controller.ts
+++ src/finance/journal-voucher/journal-voucher.controller.ts
@@ -6,33 +6,42 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
 } from '@nestjs/common';
 import { JournalVoucherService } from './journal-voucher.service';
 import { CreateJournalVoucherDto } from './dto/create-journal-voucher.dto';
 import { UpdateJournalVoucherDto } from './dto/update-journal-voucher.dto';
 import { ApiTags } from '@nestjs/swagger';
+import { JwtAuthGuard } from '../../common/guards/jwt-auth.guard';
+import { PermissionsGuard } from '../../common/guards/permissions.guard';
+import { Permissions } from '../../common/decorators/permissions.decorator';
 
 @ApiTags('Journal Voucher')
 @Controller('api/finance/journal-voucher')
+@UseGuards(JwtAuthGuard, PermissionsGuard)
 export class JournalVoucherController {
   constructor(private readonly journalVoucherService: JournalVoucherService) {}
 
   @Post()
+  @Permissions('erp.finance.journal-voucher.create')
   create(@Body() createJournalVoucherDto: CreateJournalVoucherDto) {
     return this.journalVoucherService.create(createJournalVoucherDto);
   }
 
   @Get()
+  @Permissions('erp.finance.journal-voucher.read')
   findAll() {
     return this.journalVoucherService.findAll();
   }
 
   @Get(':id')
+  @Permissions('erp.finance.journal-voucher.read')
   findOne(@Param('id') id: string) {
     return this.journalVoucherService.findOne(id);
   }
 
   @Patch(':id')
+  @Permissions('erp.finance.journal-voucher.update')
   update(
     @Param('id') id: string,
     @Body() updateJournalVoucherDto: UpdateJournalVoucherDto,
@@ -41,6 +50,7 @@ export class JournalVoucherController {
   }
 
   @Delete(':id')
+  @Permissions('erp.finance.journal-voucher.delete')
   remove(@Param('id') id: string) {
     return this.journalVoucherService.remove(id);
   }
