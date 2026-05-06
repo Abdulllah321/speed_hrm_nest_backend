09564e506077541871d9aef2d5d75b1d04701f8c
muhammad-huzaifa-anwar <muhammadhuzaifa2544@gmail.com>
2026-05-06 18:19:31 +0500

    Exchange Voucher credit Voucher s

--- src/pos-sales/pos-sales.module.ts
+++ src/pos-sales/pos-sales.module.ts
@@ -5,9 +5,10 @@ import { DatabaseModule } from '../database/database.module';
 import { StockLedgerModule } from '../warehouse/stock-ledger/stock-ledger.module';
 import { FbrService } from './fbr.service';
 import { CustomerModule } from '../sales/customer/customer.module';
+import { PosConfigModule } from '../pos-config/pos-config.module';
 
 @Module({
-    imports: [DatabaseModule, StockLedgerModule, CustomerModule],
+    imports: [DatabaseModule, StockLedgerModule, CustomerModule, PosConfigModule],
     controllers: [PosSalesController],
     providers: [PosSalesService, FbrService],
     exports: [PosSalesService],
