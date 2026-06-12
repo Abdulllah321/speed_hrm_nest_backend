0fac2ee5c73631aa3df00555f9d16b16d74db1a8
Abdulllah321 <abdullahsufyan2007@gmail.com>
2026-06-12 18:02:03 +0500

    feat: add bulk upload SSE controller and scripts for permission and role management

--- src/upload/upload.module.ts
+++ src/upload/upload.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { UploadService } from './upload.service';
 import { UploadController } from './upload.controller';
+import { BulkUploadSseController } from './bulk-upload-sse.controller';
 import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
   imports: [PrismaModule],
   providers: [UploadService],
-  controllers: [UploadController],
+  controllers: [UploadController, BulkUploadSseController],
   exports: [UploadService],
 })
 export class UploadModule {}
