f21c3d68c8d93c69a06592d1fd0bd73a4292744d
Abdulllah321 <abdullahsufyan2007@gmail.com>
2026-07-23 16:58:33 +0500

    .

--- src/pos-claims/pos-claims.module.ts
+++ src/pos-claims/pos-claims.module.ts
@@ -1,14 +1,34 @@
 import { Module } from '@nestjs/common';
+import { BullModule } from '@nestjs/bull';
 import { PosClaimsController } from './pos-claims.controller';
 import { PosClaimsService } from './pos-claims.service';
 import { DatabaseModule } from '../database/database.module';
 import { WarehouseModule } from '../warehouse/warehouse.module';
 import { StockLedgerModule } from '../warehouse/stock-ledger/stock-ledger.module';
+import { ExportHistoryModule } from '../warehouse/export-history/export-history.module';
+import { NotificationsModule } from '../notifications/notifications.module';
+import { UploadModule } from '../upload/upload.module';
+import { ClaimRegisterExportService } from './claim-register-export.service';
+import { ClaimRegisterExportProcessor } from './claim-register-export.processor';
 
 @Module({
-    imports: [DatabaseModule, WarehouseModule, StockLedgerModule],
-    controllers: [PosClaimsController],
-    providers: [PosClaimsService],
-    exports: [PosClaimsService],
+  imports: [
+    DatabaseModule,
+    WarehouseModule,
+    StockLedgerModule,
+    ExportHistoryModule,
+    NotificationsModule,
+    UploadModule,
+    BullModule.registerQueue({
+      name: 'claim-register-export',
+    }),
+  ],
+  controllers: [PosClaimsController],
+  providers: [
+    PosClaimsService,
+    ClaimRegisterExportService,
+    ClaimRegisterExportProcessor,
+  ],
+  exports: [PosClaimsService, ClaimRegisterExportService],
 })
-export class PosClaimsModule { }
+export class PosClaimsModule {}
